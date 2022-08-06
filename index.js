bfff5aa4b9e39893f6aecf93aff17272a0354aa9
josuenm <direct.josue@gmail.com>
2022-08-06 19:56:24 -0300

    Arquivo movido

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import usersRouter from "./src/routes/users.js";
+import notesRouter from "./src/routes/notes.js";
+import cors from "cors";
+import "./src/config/database.js";
+import "dotenv/config.js";
+
+const __dirname = path.resolve();
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+
+app.use("/users", usersRouter);
+app.use("/notes", notesRouter);
+
+app.listen(process.env.PORT || 8080, console.log("Server is running! 🚀"));
