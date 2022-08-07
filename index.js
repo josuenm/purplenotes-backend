68f8ee0ca1ad3b85f17469623f6d7f1eae1945bb
josuenm <direct.josue@gmail.com>
2022-08-06 23:19:38 -0300

    Middleware de controle de acesso

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import path from "path";
 import usersRouter from "./src/routes/users.js";
 import notesRouter from "./src/routes/notes.js";
-import cors from "cors";
 import "./src/config/database.js";
 import "dotenv/config.js";
 
@@ -11,14 +10,17 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  cors({
-    origin: process.env.CORS_URL,
-    credentials: false,
-  })
-);
 
-app.use("/users", usersRouter);
-app.use("/notes", notesRouter);
+const cors = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_URL);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+};
+
+app.use("/users", cors, usersRouter);
+app.use("/notes", cors, notesRouter);
 
 app.listen(process.env.PORT || 8080, console.log("Server is running! 🚀"));
