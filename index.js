b5a17747af4cd930f6518f3d41c830ba2e03279c
josuenm <direct.josue@gmail.com>
2022-08-11 15:12:03 -0300

    Middleware de controle de acesso com a variavel de ambiente, middleware do CORS foi deletado

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import path from "path";
 import usersRouter from "./src/routes/users.js";
 import notesRouter from "./src/routes/notes.js";
-import cors from "cors";
 import "./src/config/database.js";
 import "dotenv/config.js";
 
@@ -13,15 +12,9 @@ const CORS_URL = process.env.CORS_URL;
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", CORS_URL);
   next();
 });
-app.use(
-  cors({
-    credentials: false,
-    origin: CORS_URL || "*",
-  })
-);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", usersRouter);
